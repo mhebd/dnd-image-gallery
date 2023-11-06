0ac48d6620a10c844f33c6840dba84b92a3355cf
mhebd <mhemonhossain@gmail.com>
2023-11-06 21:46:07 +0600

    Changes on home page and readme.md file

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -3,13 +3,15 @@ import { NavLink } from "react-router-dom"
 function Header() {
   return (
     <header>
-      <h3>D&D Gallery</h3>
-      <nav>
-        <ul>
-          <li> <NavLink to="/">Home</NavLink> </li>
-          <li> <NavLink to="/gallery">Gallery</NavLink> </li>
-        </ul>
-      </nav>
+      <div className="container">
+        <h3>D&D Gallery</h3>
+        <nav>
+          <ul>
+            <li> <NavLink to="/">Home</NavLink> </li>
+            <li> <NavLink to="/gallery">Gallery</NavLink> </li>
+          </ul>
+        </nav>
+      </div>
     </header>
   )
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,10 @@
+import { Link } from "react-router-dom"
+
 /* eslint-disable react/no-unescaped-entities */
 function Home() {
   return (
-    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '90vh' }}>
-      <h2>Welcome to "D&D Image Gallery"</h2>
+    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '80vh' }}>
+      <h2>Welcome to "<Link to='/gallery'>D&D Image Gallery</Link>"</h2>
     </div>
   )
 }
